2d618de71918ee0ca780d09695dd506517f49519
TasfiaIslam <tasf1a027@gmail.com>
2021-07-08 18:58:41 +0600

    Handle errors

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState([]);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -10,17 +12,32 @@ const Home = () => {
     }
 
     useEffect( () => {
-        fetch("http://localhost:8000/blogs")
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
             .then(res => {
+                if(!res.ok){
+                    throw Error("Could not fetch data for that resource!");
+                }
                 return res.json()
             })
             .then((data) => {
-                setBlogs(data)
-            });
+                setBlogs(data);
+                setIsLoading(false);
+                setError([]);
+            })
+            .catch((e) => {
+                setIsLoading(false);
+                setError(e.message);
+            })
+        }, 1000)
     },[]);
 
     return ( 
         <div>
+            <div className="w-7/12 mx-auto mt-10">
+                {error && <div>{error}</div>}
+                {isLoading && <div>Loading...</div>}
+            </div>
             <BlogList blogs={blogs} handleDelete={handleDelete}/>
         </div>
      );
