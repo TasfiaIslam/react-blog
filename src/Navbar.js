cbe4799c354b0ab43b807fe402d11f00b5986c51
TasfiaIslam <tasf1a027@gmail.com>
2021-07-09 18:52:57 +0600

    Add routing

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,34 +1,36 @@
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <header class="md:flex md:items-center md:justify-between p-4 pb-0 shadow-md md:pb-4">
             <div className="w-7/12 mx-auto md:flex md:items-center md:justify-between">
                 <div class="flex items-center justify-between mb-4 md:mb-0">
                     <h1 class="leading-none text-2xl text-grey-darkest">
-                    <a class="no-underline text-green-400 font-bold hover:text-black" href="#">
+                    <Link to="/" class="no-underline text-green-400 font-bold hover:text-black">
                         My Blog
-                    </a>
+                    </Link>
                     </h1>
 
-                    <a class="text-black hover:text-orange md:hidden" href="#">
+                    <a class="text-black hover:text-orange md:hidden">
                     <i class="fa fa-2x fa-bars"></i>
                     </a>
                 </div>
                 <nav>
                     <ul class="list-reset md:flex md:items-center">
                     <li class="md:ml-4 md:mx-8">
-                        <a class="block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0" href="#">
+                        <Link to="/" class="block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
                         Blogs
-                        </a>
+                        </Link>
                     </li>
                     <li class="md:ml-4 md:mx-8">
-                        <a class="border-t block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0" href="#">
+                        <Link to="/about" class="border-t block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
                         About
-                        </a>
+                        </Link>
                     </li>
                     <li class="md:ml-4 md:mx-8">
-                        <a class="border-t block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0" href="#">
+                        <Link to="/contact" class="border-t block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
                         Contact
-                        </a>
+                        </Link>
                     </li>
                     </ul>
                 </nav>
