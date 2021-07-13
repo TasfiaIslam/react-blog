7c0071b06ed207667b192b0e74a1032e499b45de
TasfiaIslam <tasf1a027@gmail.com>
2021-07-13 22:34:27 +0600

    Search blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,27 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
-const BlogList = ({blogs}) => {
+const BlogList = ({blogs, searchTerm, searchHandler}) => {
+
+    const inputEl = useRef("");
+
+    const getSearchTerm = () => {
+        searchHandler(inputEl.current.value);
+    }
     
     return ( 
         <div className="w-7/12 mx-auto mt-10">
+            {/* Search Bar */}
+            <div class="w-1/3 shadow flex">
+                <input class="w-full rounded p-2 outline-none" type="text" placeholder="Search..."
+                        value={searchTerm} onChange={getSearchTerm} ref={inputEl}
+                     />
+                <button class="bg-white w-auto flex justify-end items-center text-blue-500 p-2 hover:text-blue-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
+            </div>
             <div className="grid grid-cols-3 gap-4">
             {blogs.map( blog => (
                 <div className="my-8 hover:shadow-md" key={blog.id}>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
 
     const {data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs");
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const searchHandler = (searchTerm) => {
+        setSearchTerm(searchTerm);
+        if(searchTerm !== ""){
+            const newBlogList = blogs.filter((blog) => {
+                return Object.values(blog)
+                        .join(" ")
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+            })
+            setSearchResults(newBlogList);
+        }
+        else{
+            setSearchResults(blogs);
+        }
+    }
 
     return ( 
         <div>
@@ -11,7 +30,7 @@ const Home = () => {
                 {error && <div>{error}</div>}
                 {isLoading && <div>Loading...</div>}
             </div>
-            <BlogList blogs={blogs} />
+            <BlogList blogs={searchTerm.length < 1 ? blogs : searchResults } searchTerm={searchTerm} searchHandler={searchHandler}/>
         </div>
      );
 }
