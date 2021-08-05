59a6152f7d6021d6bbaa3923b4c27be881fdb633
TasfiaIslam <tasf1a027@gmail.com>
2021-08-05 19:43:20 +0600

    navbar: hamburger menu in mobile view

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,45 +1,59 @@
+import { useState } from "react";
 import {Link} from 'react-router-dom';
 
 const Navbar = () => {
-    return ( 
-        <header class="md:flex md:items-center md:justify-between p-4 pb-0 shadow-md md:pb-4">
-            <div className="w-7/12 mx-auto md:flex md:items-center md:justify-between">
-                <div class="flex items-center justify-between mb-4 md:mb-0">
-                    <h1 class="leading-none text-2xl text-grey-darkest">
-                    <Link to="/" class="no-underline text-green-400 font-bold hover:text-black">
-                        WriteUp
-                    </Link>
-                    </h1>
 
-                    <a class="text-black hover:text-orange md:hidden">
-                    <i class="fa fa-2x fa-bars"></i>
-                    </a>
+    const [isHidden, setHidden] = useState("true");
+
+    const handleMenu = (e) => {
+        e.preventDefault();
+        setHidden(!isHidden);
+    }
+
+    return ( 
+        <header class="md:flex md:items-center md:justify-between py-4 pb-0 shadow-md md:pb-4">
+            <div className="w-4/5 md:w-7/12 mx-auto md:flex md:items-center md:justify-between">
+                <div className="flex items-center justify-between">
+                    
+                    <div class="flex items-center justify-between mb-4 md:mb-0">
+                        <h1 class="leading-none text-2xl text-grey-darkest">
+                            <Link to="/" class="no-underline text-green-400 font-bold hover:text-black">
+                                WriteUp
+                            </Link>
+                        </h1>
+                    </div>
+
+                    <nav className="md:hidden mb-4" onClick={handleMenu}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </nav>
                 </div>
-                <nav className="">
+                <nav className={`md:block ${isHidden ? "hidden" : "block"} `}>
                     <ul class="list-reset md:flex md:items-center">
-                    <li class="md:ml-4 md:mx-8">
-                        <Link to="/blogs/add" class="block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
-                        New Blog
-                        </Link>
-                    </li>
-                    <li class="md:ml-4 md:mx-8">
-                        <Link to="/" class="block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
-                        Blogs
-                        </Link>
-                    </li>
-                    <li class="md:ml-4 md:mx-8">
-                        <Link to="/about" class="border-t block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
-                        About
-                        </Link>
-                    </li>
-                    <li class="md:ml-4 md:ml-4">
-                        <Link to="/contact" class="border-t block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
-                        Contact
-                        </Link>
-                    </li>
+                        <li class="md:ml-4 md:mx-8">
+                            <Link to="/blogs/add" class="block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
+                            New Blog
+                            </Link>
+                        </li>
+                        <li class="md:ml-4 md:mx-8">
+                            <Link to="/" class="block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
+                            Blogs
+                            </Link>
+                        </li>
+                        <li class="md:ml-4 md:mx-8">
+                            <Link to="/about" class="block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
+                            About
+                            </Link>
+                        </li>
+                        <li class="md:ml-4 md:ml-4">
+                            <Link to="/contact" class="block no-underline hover:underline font-semibold py-2 text-gray-400 hover:text-black md:border-none md:p-0">
+                            Contact
+                            </Link>
+                        </li>
                     </ul>
                 </nav>
-            </div>
+                </div>
         </header>
     );
 }
