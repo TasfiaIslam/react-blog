f66d0e9f08f05c8ab332851e02696a89037d79b3
TasfiaIslam <tasf1a027@gmail.com>
2021-08-08 13:42:29 +0600

    Add category dropdown search

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,26 +1,48 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-const BlogList = ({blogs, searchTerm, searchHandler}) => {
+const BlogList = ({blogs, searchTerm, searchCategory, searchHandler, categoryHandler}) => {
 
+    
     const inputEl = useRef("");
+    const selectEl = useRef("");
 
-    const getSearchTerm = () => {
+    const getSearchItems = () => {
         searchHandler(inputEl.current.value);
+    }
+
+    const getSelectItems = () => {
+        categoryHandler(selectEl.current.value);
     }
     
     return ( 
         <div className="w-4/5 md:w-7/12 mx-auto md:mt-10">
             {/* Search Bar */}
-            <div class="mb-4 md:mb-0 md:w-1/3 shadow flex">
-                <input class="w-full rounded p-2 outline-none" type="text" placeholder="Search..."
-                        value={searchTerm} onChange={getSearchTerm} ref={inputEl}
-                     />
-                <button class="bg-white w-auto flex justify-end items-center text-blue-500 p-2 hover:text-blue-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
-                </button>
+            <div className="flex justify-between">
+                <div className="mb-4 md:mb-0 md:w-1/3 shadow flex">
+                    <input className="w-full rounded p-2 outline-none" type="text" placeholder="Search..."
+                            value={searchTerm} onChange={getSearchItems} ref={inputEl}
+                        />
+                    <button className="bg-white w-auto flex justify-end items-center text-blue-500 p-2 hover:text-blue-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </button>
+                </div>
+                {/* Select Category */}
+                <div>
+                    <select
+                        value={searchCategory} ref={selectEl}
+                        onChange={getSelectItems}
+                        className="w-6/7 h-10 border-none outline-none shadow-md bg-white"
+                    >
+                        <option value="None">Category</option>
+                        <option value="Technology">Technology</option>
+                        <option value="Sports">Sports</option>
+                        <option value="Beauty">Beauty</option>
+                        <option value="Travel">Travel</option>
+                    </select>
+                </div>
             </div>
             <div className="grid md:grid-cols-3 gap-4">
             {blogs.map( blog => (
--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,36 @@ import useFetch from "./useFetch";
 const Home = () => {
     // https://my-json-server.typicode.com/tasfiaislam/blog-json-server/blogs/
     const {data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs");
+    
     const [searchTerm, setSearchTerm] = useState('');
+    const [searchCategory, setSearchCategory] = useState("None");
     const [searchResults, setSearchResults] = useState([]);
 
+    const [searchArray, setSearchArray] = useState([]);
+
+
+    const categoryHandler = (searchCategory) => {
+
+        setSearchCategory(searchCategory);
+
+        if(searchCategory !== "None"){
+            const newBlogList = blogs.filter( (blog) => (
+                blog.category.toLowerCase() === searchCategory.toLowerCase()
+             ));
+             setSearchArray(newBlogList);
+             setSearchResults(newBlogList);
+        }else{
+            setSearchArray(blogs);
+            setSearchResults(blogs);
+        }       
+    }
+
     const searchHandler = (searchTerm) => {
-        setSearchTerm(searchTerm);
+
+        setSearchTerm(searchTerm);   
+
         if(searchTerm !== ""){
-            const newBlogList = blogs.filter((blog) => {
+            const newBlogList = searchArray.filter((blog) => {
                 return Object.values(blog)
                         .join(" ")
                         .toLowerCase()
@@ -30,7 +53,11 @@ const Home = () => {
                 {error && <div>{error}</div>}
                 {isLoading && <div>Loading...</div>}
             </div>
-            <BlogList blogs={searchTerm.length < 1 ? blogs : searchResults } searchTerm={searchTerm} searchHandler={searchHandler}/>
+            <BlogList 
+                blogs={(searchTerm.length < 1 && searchCategory === "None") ? blogs : searchResults } 
+                searchTerm={searchTerm} searchCategory={searchCategory} searchHandler={searchHandler}
+                categoryHandler={categoryHandler}
+            />
         </div>
      );
 }
